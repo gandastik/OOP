82204cbd4c4e2789b672f3a3d78b791fa97c2f31
gandastik <gandastik@gmail.com>
2021-03-28 22:13:40 +0700

    ➕ add Colorable interface, square

---
+++ Lab08/Square.java
@@ -0,0 +1,15 @@
+public class Square extends Geometric implements Colorable {
+    private double side;
+
+    public Square() {
+
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public void howToColor() {
+    }
+
+}
